f523ea9c2843db8b843338fecacfa353ba839dcf
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-09-11 15:10:00 +0530

    e

--- vite-project/src/Task/FormPrompt.jsx
+++ vite-project/src/Task/FormPrompt.jsx
@@ -9,14 +9,32 @@ const FormPrompt = () => {
     const [username, setUserName] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSignup = (e) => {
         e.preventDefault();
 
         localStorage.setItem('name', name);
         localStorage.setItem('username', username);
         localStorage.setItem('password', password);
 
-        if (username === localStorage.getItem('username', username) && password === localStorage.getItem('password', password)) {
+        setName('');
+        setUserName('');
+        setPassword('');
+
+        // toast.success('Signup success!', {
+        //     position: "top-right",
+        //     autoClose: 5000,
+        // });
+
+    }
+
+
+    const [user, setUser] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        if (user === localStorage.getItem('username') && pass === localStorage.getItem('password')) {
             toast.success('Login success!', {
                 position: "top-right",
                 autoClose: 5000,
@@ -34,44 +52,42 @@ const FormPrompt = () => {
     return (
         <div>
 
-            <form onSubmit={handleSubmit}>
-                <h1>Signup</h1>
+            <div className='formdiv' style={{display:"flex", justifyContent:"space-evenly"}}>
+                <form onSubmit={handleSignup}  style={{width:"30%", backgroundColor:"teal", padding: "20px 20px 50px 20px", marginTop:"50px"}}>
+                    <h1>Signup</h1>
 
-                <label htmlFor="name">Name:</label>
-                <input type="text" value={name} onChange={(e) => setName(e.target.value)} required /><br /><br />
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} required /><br /><br />
 
-                <label htmlFor="username">Username:</label>
-                <input type="text" value={username} onChange={(e) => setUserName(e.target.value)} required /><br /><br />
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" value={username} onChange={(e) => setUserName(e.target.value)} required /><br /><br />
 
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required /><br /><br />
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required /><br /><br />
 
-                <button type="submit">Submit</button>
+                    <button type="submit">Signup</button>
 
-            </form>
+                </form>
 
-            <br />
-            <br />
-            <br />
-            <form onSubmit={handleSubmit}>
+                <form onSubmit={handleLogin}  style={{width:"30%", backgroundColor:"green", padding: "20px 20px 50px 20px", marginTop:"50px"}}>
 
-                <h1>Login</h1>
-                <label htmlFor="username">Username:</label>
-                <input type="text" value={username} onChange={(e) => setUserName(e.target.value)} required /><br /><br />
+                    <h1>Login</h1>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" value={user} onChange={(e) => setUser(e.target.value)} required /><br /><br />
 
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required /><br /><br />
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" value={pass} onChange={(e) => setPass(e.target.value)} required /><br /><br />
 
-                <button type="submit">Submit</button>
+                    <button type="submit">Login</button>
 
-            </form>
-
-            <div>
-                <h2>Welcome {name}!</h2>
-                <p>Your password is {password}</p>
+                </form>
             </div>
 
-            <ToastContainer/>
+
+
+            <ToastContainer />
+
+            
 
         </div>
     )
